8a107ae4bfa9a44f8ac9d1d4dbf69d70b35f1899
Silasprata23 <silasprata23@gmail.com>
2026-05-05 22:50:15 -0300

    feat:creation of the component card

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,8 +1,43 @@
+import {Button} from "../button/index.jsx";
+import "./style.scss"
 
-export const Card = () => {
+export const Card = ({tipo, variant, title, text, button, linkButton, img}) => {
 
-    <div>
-        
-    </div>
+    return (
+
+
+
+        <div className="card-wrapper">
+
+            {tipo ==='title' && (
+                <div className="card-title">
+                    <h3>{title}</h3>
+                </div>
+            )}
+
+
+
+            {tipo === 'services' && (
+                <div className={variant}>
+                    <h3>{title}</h3>
+                    <p>{text}</p>
+                    {img && <img src={img} alt=""/>}
+                    {button && <Button text={button}
+                    link={linkButton}/>}
+                </div>
+            ) }
+
+
+            {tipo === 'socialmedia' && (
+                <div className="card-media">
+                    <p>{text}</p>
+                    <Button text={linkButton}/>
+                    <Button text={linkButton}/>
+                </div>
+            )}
+
+
+        </div>
+    )
 
 }
