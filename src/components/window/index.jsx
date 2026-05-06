e6f44935103819dcbe756de3a6aeae42ef680ae1
Silasprata23 <silasprata23@gmail.com>
2026-05-05 22:54:37 -0300

    feat:component window completion

--- src/components/window/index.jsx
+++ src/components/window/index.jsx
@@ -1,8 +1,23 @@
+import close from "../../assets/svg/button_close.svg"
+import "./style.scss"
+import {Card} from "../card/index.jsx";
+
 
 export const Window = () => (
     <div className="window">
-        <div className="TitleBar">
+        <div className="titleBar">
+            <a href="">
+                <img src={close} alt="red-button"/>
+            </a>
 
         </div>
+        {/*tava so testando boy */}
+        <Card tipo={"services"}
+              title={"UI / UX Design para Produtos Digitais"}
+              text={"Transformamos ideias em interfaces intuitivas, responsivas e com foco na experiência do usuário projetadas para escalar globalmente."}
+              button={"Solicitar Orçamento "}
+              linkButton={""}
+        />
+
     </div>
 )
