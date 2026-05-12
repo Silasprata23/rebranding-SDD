d9df43c4911e1cc7dbb310c18e364b0fb25e6b4c
Silasprata23 <silasprata23@gmail.com>
2026-05-12 11:03:52 -0300

    fix: update CardBig component styles and adjust color classes

--- src/components/card-big/index.jsx
+++ src/components/card-big/index.jsx
@@ -1,9 +1,10 @@
 import {Button} from "../button/index.jsx";
+import "./style.scss"
 
 
 export const CardBig = ({ color, title, text, button, linkButton}) => {
     return (
-        <div className={color ? "color1" : "color2"}>
+        <div className={color ? "color1" : "color4"}>
             <div className="text">
                 <h3>{title}</h3>
                 <p>{text}</p>
