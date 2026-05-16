130309836e92756d35479fdd67f65e97392ceafb
Silasprata23 <silasprata23@gmail.com>
2026-05-16 18:08:55 -0300

    feat: add CardSlogan component with styles

---
+++ src/components/card-slogan/index.jsx
@@ -0,0 +1,35 @@
+import "./style.scss"
+
+
+export const CardSlogan = () => {
+
+    return(
+        
+         <div className="card-slogan">
+                    <div className="slogan">
+                        
+                    <p className="left">
+                        <span>Empresas sólidas</span>
+                    </p>
+                    
+                    <p className="right"> 
+                        <span>precisam ⌘</span>
+                    </p>
+                    <p className="left">
+                         <span>de um sistema</span>
+                    </p>
+                   <p className="right"> 
+                     <span>escalável ↗</span> 
+                   </p>
+                    
+
+                    </div>
+                    
+
+                </div>
+
+
+    )
+        
+}
+
